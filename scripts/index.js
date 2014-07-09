2b47985e562e8bce720719ba1d7a1ad96c138dcb
Indeed <indeed@indeedblog.net>
2014-07-09 20:24:51 +0800

    implement parser.

--- scripts/index.js
+++ scripts/index.js
@@ -39,8 +39,31 @@ function nextStep() {
 }
 
 function initialize(cars, steps) {
-    var board = new Board(cars);
-    replay = new Replay(steps, document.getElementById('replay-canvas'), board);
+    replay = QingBiaoParser(
+        [   '2,2,4,0' ,
+            '3,1,6,1' ,
+            '2,2,6,1' ,
+            '3,4,6,1' ,
+            '2,5,6,1' ,
+            '2,6,6,1' ,
+            '2,3,3,1' ,
+            '3,4,3,0' ,
+            '3,3,1,0' ,
+            '2,6,2,1'
+        ], '6,4',
+        [
+            '2 1 3',
+            '1 1 1',
+            '7 0 3',
+            '1 0 1',
+            '2 0 3',
+            '8 1 3',
+            '9 1 2',
+            '4 1 3',
+            '1 0 3'
+        ]
+    );
+
     setInterval(redrawAll, ANIMATION_INTERVAL / 50);
 }
 
--- scripts/parser.js
+++ scripts/parser.js
@@ -1,11 +1,33 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: indeed
- * Date: 14-7-9
- * Time: 下午7:48
- */
-function QingBiaoParser(initialPosition , exitPosition, steps)
-{
-
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+function QingBiaoParser(initialPosition, exitPosition, stepsOutput) {
+    var cars = [];
+    for (var i = 0; i < initialPosition.length; ++i) {
+        var s = initialPosition[i];
+        var a = s.split(',');
+        cars.push(
+            new Car(i + 1, parseInt(a[3]) ? 1 : parseInt(a[0]), parseInt(a[3]) ? parseInt(a[0]) : 1, parseInt(a[1]), parseInt(a[2])
+                , getRandomColor()
+            )
+        );
+    }
+    var steps = [];
+    for (var i = 0; i < stepsOutput.length; ++i) {
+        var s = stepsOutput[i];
+        var a = s.split(' ');
+        for (var j = 0; j < parseInt(s[2]); ++j)
+            steps.push(new Step(parseInt(a[0]), parseInt(a[1]) ? 'backward' : 'forward'));
+    }
+    var board = new Board(cars);
+    var replay = new Replay(steps, document.getElementById('replay-canvas'), board);
+    var a = exitPosition.split(',');
+    replay.exitX = parseInt(a[0]);
+    replay.exitY = parseInt(a[1]);
+    return replay;
 }
